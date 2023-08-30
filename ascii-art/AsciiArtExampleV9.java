56517404ec7e998e06e8c1498db72a4a39cb6961
kdev0x <119963213+kdev0x@users.noreply.github.com>
2023-08-30 09:27:58 -0700

    Create AsciiArtExampleV9.java

---
+++ ascii-art/AsciiArtExampleV9.java
@@ -0,0 +1,79 @@
+class Board{
+    private int rows;
+    private int cols;
+    private char[][] board;
+    private char defaultSymbol;
+
+    public Board(int rows, int cols, char defaultSymbol){
+        this.rows = rows;
+        this.cols = cols;
+        this.defaultSymbol = defaultSymbol;
+        this.board = new char[this.rows][this.cols];
+        this.fillBoard();
+    }
+    
+    public Board(int size, char defaultSymbol){
+        this.rows = size;
+        this.cols = size;
+        this.defaultSymbol = defaultSymbol;
+        this.board = new char [this.rows][this.cols];
+        this.fillBoard();
+    }
+
+    private void fillBoard(){
+        for(int i = 0; i < this.rows; i++){
+            for(int ii = 0; ii < this.cols; ii++){
+                this.board[i][ii] = this.defaultSymbol;
+            }
+        }
+    }
+
+    public char[][] getBoard(){
+        return this.board;
+    }
+
+    public char getDefaultSymbol(){
+        return this.defaultSymbol;
+    }
+
+    public int getRows(){
+        return this.rows;
+    }
+
+    public int getCols(){
+        return this.cols;
+    }
+
+    public boolean setValue(int row, int col, char val){
+        if(row < this.rows && col < this.cols){
+            this.board[row][col] = val;
+            return true;
+        }
+        return false;
+    }
+
+    public void draw(){
+        for(int i = 0; i < this.rows; i++){
+            for(int ii = 0; ii < this.cols; ii++){
+                System.out.print(this.board[i][ii]);
+            }
+            System.out.println("");
+        }
+
+    }
+}
+
+public class AsciiArtExampleV9{
+    public static void main(String[] args) {
+        int asciiCode = 65;
+        Board board = new Board(8, '.');
+        for(int i = 0; i < board.getRows() ; i++){
+            for(int ii = 0; ii < board.getCols(); ii++){
+                board.setValue(i, ii, (char)asciiCode);
+                asciiCode++;
+            }
+        }
+        board.draw();
+    }
+
+}
